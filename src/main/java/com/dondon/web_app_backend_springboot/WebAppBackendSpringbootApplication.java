8d3f8580371ca37cebe29ee331a73f6f52fdb814
dondonjeric.co <dondonco@ibm.com>
2025-11-08 09:01:30 +0800

    first commit

---
+++ src/main/java/com/dondon/web_app_backend_springboot/WebAppBackendSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.dondon.web_app_backend_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebAppBackendSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebAppBackendSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dondon/web_app_backend_springboot/WebAppBackendSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dondon.web_app_backend_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebAppBackendSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
